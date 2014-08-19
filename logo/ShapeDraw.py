c740c74965d9972f7aa857a0ae662a2aed3d962a
pb471 <pb471@cam.ac.uk>
2014-08-19 12:00:12 +0100

    Running logo/ShapeDraw.py

--- logo/ShapeDraw.py
+++ logo/ShapeDraw.py
@@ -5,6 +5,8 @@ def Draw(Name, Colour, Size):
     DrawSquare(Colour, Size)
   elif Name == "Triangle"
     DrawTriangle(COlour, Size)
+  end
+
     
 def PolygonDraw(Colour, Size, Sides)
   color(Colour)
