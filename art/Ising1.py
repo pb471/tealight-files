afa2da2543b3bc3d5d916a5b50b5f45198161e09
pb471 <pb471@cam.ac.uk>
2014-08-19 12:25:58 +0100

    Running art/Ising1.py

--- art/Ising1.py
+++ art/Ising1.py
@@ -3,7 +3,13 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 from tealight.art import (screen_width, screen_height)
 
 #Classdefs
-x.Var1 = 1
 
-print x
+#Function definitions
+def create_matrix(i_max, j_max, DefaultValue=0):
+  Matrix = [[0 for x in xrange(i_max)] for x in xrange(j_max)]
+  for i in range(0, i_max):
+    for j in range(0, j_max):
+      Matrix[i][j] = DefaultValue
+     
+
 
