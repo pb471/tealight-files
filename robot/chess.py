e20dc6ea2cabb1713fe7cab8ac00e9cd9fbe7e61
pb471 <pb471@cam.ac.uk>
2014-08-19 14:29:42 +0100

    Running robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -26,13 +26,6 @@ for j in range(0,4):
        move()
   turn(1)
   
-  for j in range(0,4):
-  for i in range(0,45):
-    if touch() == "wall":
-      turn(2)
-    else:
-       move()
-  turn(1)
   
 for j in range(0,4):
   for i in range(0,50):
