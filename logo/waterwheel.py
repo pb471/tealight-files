c60f327b48ce439f44392117e4ca34d6bd803645
pb471 <pb471@cam.ac.uk>
2014-08-19 14:02:48 +0100

    Running logo/waterwheel.py

--- logo/waterwheel.py
+++ logo/waterwheel.py
@@ -11,8 +11,16 @@ def waterwheel(edges, size):
   decoration = size / 4
   for i in range(0, edges):
     move(size)
-    square(decoration)
+    #square(decoration)
+    move(decoration)
     turn(angle)
 
+def circle(side):
+  N = 100
+  angle = 360.0/N
+  for i in range(0, N)
+    move(side)
+    turn(angle)
+    
 turn(-90)
 waterwheel(12,100)
