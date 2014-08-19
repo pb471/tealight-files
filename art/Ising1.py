52865719651a2c3c40f7bf20eb7040fe1f243464
pb471 <pb471@cam.ac.uk>
2014-08-19 12:27:58 +0100

    Running art/Ising1.py

--- art/Ising1.py
+++ art/Ising1.py
@@ -12,6 +12,6 @@ def create_matrix(i_max, j_max, DefaultValue=0):
       Matrix[i][j] = DefaultValue
   return Matrix
      
-Mat1 = create_matrix(5,2,7)
+Mat1 = create_matrix(3,3,7)
 print Mat1[3][3]
 
