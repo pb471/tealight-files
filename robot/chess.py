869e97cc697c476be865e4e4e179e961b4cf8009
pb471 <pb471@cam.ac.uk>
2014-08-19 14:42:28 +0100

    Running robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -20,14 +20,6 @@ for j in range(0,4):
       move()
       move()
 
-for j in range(0,4):
-  for i in range(0,50):
-    move()
-    if touch() == "wall":
-      turn(1)
-      move()
-      move()
-      move()
-      move()
+
        
 
