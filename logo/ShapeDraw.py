2fb3b2ae43dd9f914844f38cbf2a644119704e39
pb471 <pb471@cam.ac.uk>
2014-08-19 12:00:38 +0100

    Running logo/ShapeDraw.py

--- logo/ShapeDraw.py
+++ logo/ShapeDraw.py
@@ -3,9 +3,6 @@ print "Welcome to ShapeDraw 1.0"
 def Draw(Name, Colour, Size):
   if Name == "Square":
     DrawSquare(Colour, Size)
-  elif Name == "Triangle"
-    DrawTriangle(COlour, Size)
-  end
 
     
 def PolygonDraw(Colour, Size, Sides)
