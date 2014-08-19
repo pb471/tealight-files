06d3efa19905b862ed4432bd0cc4138c833d8ae7
pb471 <pb471@cam.ac.uk>
2014-08-19 12:20:33 +0100

    Running art/Ising1.py

--- art/Ising1.py
+++ art/Ising1.py
@@ -1,1 +1,9 @@
-print "A new file!"
+#Import stuff
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+from tealight.art import (screen_width, screen_height)
+
+#Classdefs
+x.Var1 = 1
+
+print x
+
