a21cacf86cf5e7aec9b73f53255424daf003660b
pb471 <pb471@cam.ac.uk>
2014-08-20 09:53:55 +0100

    Running art/Ising1.py

--- art/Ising1.py
+++ art/Ising1.py
@@ -35,7 +35,7 @@ class Lattice:
   def GetSpins(self, x, y):
     S = [0, 0, 0, 0, 0]
     S[0] = self.Array[x][y]
-    S[1] = self.GetSpinCyclicBC(self.Array, x+1,y)
+    S[1] = self.GetSpinCyclicBC(x+1,y)
     S[2] = self.GetSpinCyclicBC(self.Array, x-1,y)
     S[3] = self.GetSpinCyclicBC(self.Array, x,y+1)
     S[4] = self.GetSpinCyclicBC(self.Array, x,y-1)
