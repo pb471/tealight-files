c80a7cb03741353b097bcb279e2fab3af24d884a
pb471 <pb471@cam.ac.uk>
2014-08-20 09:12:49 +0100

    Running art/Ising1.py

--- art/Ising1.py
+++ art/Ising1.py
@@ -64,7 +64,7 @@ def print_magnetisation(s):
   text(600,600, z)
   
 #Test frame event
-def handle_keypress("a"):
+def handle_keydown("a"):
   clear_screen()
   s = Lattice(12, 0.5)
   print_magnetisation(s)
