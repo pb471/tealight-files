6b513956f77a87632f2363356f076ed5c79b090a
pb471 <pb471@cam.ac.uk>
2014-08-20 09:55:15 +0100

    Running art/Ising1.py

--- art/Ising1.py
+++ art/Ising1.py
@@ -138,7 +138,7 @@ def handle_keydown(a):
   
 #####################
 #Main
-P = Params(14, #Side length
+P = Params(5, #Side length
            0.5, #Fraction spin-up
            1, #J
            1, #T
