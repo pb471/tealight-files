b6cfddca542ab5d21721143fa1ca91324980462b
pb471 <pb471@cam.ac.uk>
2014-08-20 10:56:55 +0100

    Running art/Ising1.py

--- art/Ising1.py
+++ art/Ising1.py
@@ -167,9 +167,9 @@ def print_params(P):
 #Handle keypresses
 def handle_keydown(key):
   if key == "q":
-    keys{"q" : 1}
+    keys = {"q" : 1}
   if key == "a":
-    keys{"a"} : 1}
+    keys = {"a"} : 1}
     
 def handle_keyup(key):
   if key == "q":
