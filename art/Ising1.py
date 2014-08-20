44705681e90bdc7f4a4104eed5b1b55528b57146
pb471 <pb471@cam.ac.uk>
2014-08-20 12:17:58 +0100

    Running art/Ising1.py

--- art/Ising1.py
+++ art/Ising1.py
@@ -198,7 +198,7 @@ def handle_frame():
   
 #####################
 #Main
-P = Params(25, #Side length
+P = Params(18, #Side length
            1, #Fraction spin-up
            -1, #J
            2, #T
