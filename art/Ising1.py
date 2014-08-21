9d50fe43e68647a8b6b7386b25422b00df590b15
pb471 <pb471@cam.ac.uk>
2014-08-21 11:58:39 +0100

    Running art/Ising1.py

--- art/Ising1.py
+++ art/Ising1.py
@@ -299,7 +299,7 @@ P = Params(20, #Side length
            -1, #J
            2, #T
            0, #Field
-           200, #Metropolis steps per key press
+           5, #Metropolis steps per key press
            1, #Sleep time
            "blue", #spin-up colour
            "green") #spin-down colour
