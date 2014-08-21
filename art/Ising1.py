d7004e289876689666fcab9767a3d20110047c4f
pb471 <pb471@cam.ac.uk>
2014-08-21 10:35:30 +0100

    Running art/Ising1.py

--- art/Ising1.py
+++ art/Ising1.py
@@ -77,7 +77,7 @@ class Lattice:
       self.Array[x][y] = -self.Array[x][y]
       
     if old_spin != self.Array[x][y]:
-      self.spins_changed = 
+      a =1
   
   #Returns a vector of 5 spins: a spin and its nearest neighbours
   def GetSpins(self, x, y):
