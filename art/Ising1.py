cd5d6518c0e18eb6d366424b4b7bfe32de9e15bf
pb471 <pb471@cam.ac.uk>
2014-08-21 15:31:58 +0100

    Running art/Ising1.py

--- art/Ising1.py
+++ art/Ising1.py
@@ -358,7 +358,7 @@ Colour2 = [0, 125, 0, 1]
 ColourString1 = coltostr(Colour1)
 ColourString2 = coltostr(Colour2)
 
-P = Params(9, #Side length
+P = Params(20, #Side length
            0.5, #Fraction spin-up
            -1, #J
            2, #T
