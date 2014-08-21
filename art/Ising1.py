d66afb81d682c27c0a4c2a980d515f5f80dce660
pb471 <pb471@cam.ac.uk>
2014-08-21 10:50:20 +0100

    Running art/Ising1.py

--- art/Ising1.py
+++ art/Ising1.py
@@ -178,7 +178,7 @@ def create_matrix(i_size, j_size):
   Matrix = [[0 for x in xrange(j_size)] for x in xrange(i_size)]
   for i in range(0, i_size):
     for j in range(0, j_size):
-      Matrix[i][j] = 0
+      Matrix[i][j] = i*j
   return Matrix
 
 #Wipe screen
