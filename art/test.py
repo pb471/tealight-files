54c647e0e56cc0855a11858fe87d49f760fc76e3
pb471 <pb471@cam.ac.uk>
2014-08-21 12:22:05 +0100

    Running art/test.py

--- art/test.py
+++ art/test.py
@@ -1,5 +1,5 @@
-print "A new file!"
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+from tealight.art import (screen_width, screen_height)
 
-s = [1, 2, 3]
-q = sum(s)
-print q
+color("rgba(255,0,0,0.5)")
+box(10,10, 100, 100)
