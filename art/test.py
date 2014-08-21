6e3d90b5c5b40d782d096c80e45feba2acd5fedd
pb471 <pb471@cam.ac.uk>
2014-08-21 12:24:05 +0100

    Running art/test.py

--- art/test.py
+++ art/test.py
@@ -1,7 +1,7 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 from tealight.art import (screen_width, screen_height)
 
-color("rgba(255,0,0,0.05)")
+color("rgba(255,0,0,0.5)")
 box(10,10, 100, 100)
-color("rgba(0,255,0,0.05)")
+color("rgba(0,255,0,0.5)")
 box(10,10, 100, 100)
