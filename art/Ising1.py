633d3ea35be727d5d02154992482a3d5ebe4ca7e
pb471 <pb471@cam.ac.uk>
2014-08-21 12:27:45 +0100

    Running art/Ising1.py

--- art/Ising1.py
+++ art/Ising1.py
@@ -237,8 +237,8 @@ def draw_spins(render_spin_changed, N):
 
 #Draw a square showing the lattice's magnetisation    
 def draw_mag_square(col1, col2, M):
-  pos = [630, 200]
-  size = [80,80]
+  pos = [600, 200]
+  size = [150,150]
   color("white")
   box(pos[0], pos[1], size[0], size[1])
   alpha1 = 0.5*(M+1)
