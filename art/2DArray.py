e5d6cac66bc444f55223d97f431e9fa6a84f8cd5
pb471 <pb471@cam.ac.uk>
2014-08-22 11:03:33 +0100

    Running art/2DArray.py

--- art/2DArray.py
+++ art/2DArray.py
@@ -176,12 +176,10 @@ M.components_randint(-5,5)
 M.draw_debug([30, 550], [200, 200])
 
 #Nice colourmap
-color("white")
-box(0,0, screen_width, screen_height)
 M = Array2D(100,100)
 for i in range(0,100):
   for j in range(0,100):
-    M[i,j] = -50 + i + j
+    M[i,j] = -100 + i + j
     
 M.draw()
     
