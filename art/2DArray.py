702336ccdc87579207e8ca44e128fcf4a3eed892
pb471 <pb471@cam.ac.uk>
2014-08-22 09:23:49 +0100

    Running art/2DArray.py

--- art/2DArray.py
+++ art/2DArray.py
@@ -8,7 +8,7 @@ class TwoDArray:
   def __init__(self, i_size, j_size, default_value = 0):
     self.i_size = i_size
     self.j_size = j_size
-    self.Matrix = self.create_matrix(i_size, j_size, default_value)
+    self.Array= self.create_matrix(i_size, j_size, default_value)
   
   #Return an i by j matrix, all components set to default_value
   def create_matrix(self, i_size, j_size, default_value):
