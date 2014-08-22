da4278b786f75ab8b9d521365038b9baf9bf77c3
pb471 <pb471@cam.ac.uk>
2014-08-22 09:21:08 +0100

    Closing art/2DArray.py

--- art/2DArray.py
+++ art/2DArray.py
@@ -4,4 +4,11 @@ from tealight.art import (screen_width, screen_height)
 
 #Classdefs
 class 2DArray:
+  def __init__(self, i_size, j_size, default_value = 0):
+    self.i_size = i_size
+    self.j_size = j_size
+    self.Matrix = self.create_matrix(i_size, j_size, default_value)
   
+  def create_matrix(self, i_size, j_size, default_value):
+    
+    
